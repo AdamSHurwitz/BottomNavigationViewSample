820dc10c4cc52bfdb696991caf2b12abb35b6899
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-05-15 14:46:22 -0700

    Fix: Added parent navigation graph with nested graphs for the BottomNavigationView menu items.

--- app/src/main/java/com/example/bottomnavigationviewsample/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationviewsample/MainActivity.kt
@@ -13,13 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.home))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.home, R.id.saved))
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
         val navController = navHostFragment.navController
-
-        // TODO: Attempted solution.
-        //val navController = findNavController(R.id.nav_host_container)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottom_nav.setupWithNavController(navController)
